ff68c916cd438df4c14d781ac6ca36b668b1228e
alvin8313 <alvin8313@126.com>
2023-05-08 10:03:28 +0800

    4

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -98,7 +98,7 @@ export function SideBar(props: { className?: string }) {
       <div className={styles["sidebar-header"]}>
         <div className={styles["sidebar-title"]}>华同AI/div>
         <div className={styles["sidebar-sub-title"]}>
-          GPT-4能力已启用,请购买KEY获取使用权.
+          已接入GPT-4能力,请购买KEY使用
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
