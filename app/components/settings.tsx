6aada7f6dc779f42b479ebcbca568bd578a3a069
alvin.fu <alvin8313@126.com>
2023-05-30 12:09:26 +0800

    0530-01

--- app/components/settings.tsx
+++ app/components/settings.tsx
@@ -517,30 +517,29 @@ export function Settings() {
               />
             </ListItem>
           ) : null}
-
-          <ListItem
-            title={Locale.Settings.Usage.Title}
-            subTitle={
-              showUsage
-                ? loadingUsage
-                  ? Locale.Settings.Usage.IsChecking
-                  : Locale.Settings.Usage.SubTitle(
-                      usage?.used ?? "[?]",
-                      usage?.subscription ?? "[?]",
-                    )
-                : Locale.Settings.Usage.NoAccess
-            }
-          >
-            {!showUsage || loadingUsage ? (
-              <div />
-            ) : (
-              <IconButton
-                icon={<ResetIcon></ResetIcon>}
-                text={Locale.Settings.Usage.Check}
-                onClick={() => checkUsage(true)}
-              />
-            )}
-          </ListItem>
+//          <ListItem
+//            title={Locale.Settings.Usage.Title}
+//            subTitle={
+//              showUsage
+//                ? loadingUsage
+//</List>                  ? Locale.Settings.Usage.IsChecking
+//                  : Locale.Settings.Usage.SubTitle(
+//                      usage?.used ?? "[?]",
+//</div>                      usage?.subscription ?? "[?]",
+//</ErrorBoundary>                    )
+//                : Locale.Settings.Usage.NoAccess
+//            }
+//          >
+//            {!showUsage || loadingUsage ? (
+//              <div />
+//            ) : (
+//              <IconButton
+//                icon={<ResetIcon></ResetIcon>}
+//                text={Locale.Settings.Usage.Check}
+//                onClick={() => checkUsage(true)}
+//              />
+//            )}
+//          </ListItem>
         </List>
 
         <List>
